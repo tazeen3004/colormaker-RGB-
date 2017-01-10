0522d4ab951119714c52963675f1edd065e880bd
tazeen khan <tazeenfirojkhan@gmail.com>
2017-01-10 17:21:06 +0530

    RGB values and colour displayed

--- colormaker(RGB)/ViewController.swift
+++ colormaker(RGB)/ViewController.swift
@@ -11,9 +11,54 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var RedLabel: UILabel!
+    
+    @IBOutlet weak var GreenLabel: UILabel!
+    @IBOutlet weak var BlueLabel: UILabel!
+    @IBOutlet weak var RedSlider: UISlider!
+    @IBOutlet weak var GreenSlider: UISlider!
+    @IBOutlet weak var BlueSlider: UISlider!
+    @IBOutlet weak var DisplayingLabel: UILabel!
+    
+    var RedColor :Float = 0
+    var GreenColor :Float = 0
+    var BlueColor :Float = 0
+    
+    @IBAction func RedSliderAction(_ sender: UISlider) {
+        ChangeColor()
+    }
+    
+    @IBAction func GreenSliderAction(_ sender: UISlider) {
+        ChangeColor()    }
+    
+    @IBAction func BlueSliderAction(_ sender: UISlider) {
+    ChangeColor()
+    }
     
     
     
+    func ChangeDislayLabelColor(){
+        
+        DisplayingLabel.backgroundColor = UIColor(red: CGFloat(RedColor), green: CGFloat(GreenColor), blue:CGFloat(BlueColor), alpha: 1.0)
+        ChangeNumberlabel()
+    }
+    
+    func ChangeColor(){
+        RedColor = RedSlider.value
+        GreenColor = GreenSlider.value
+        BlueColor = BlueSlider.value
+        ChangeDislayLabelColor()
+    }
+    
+    func ChangeNumberlabel(){
+        let RoundedRed = String(format: "%0.0f",(RedColor * 255))
+        let RoundedGreen = String(format: "%0.0f",(GreenColor * 255))
+        let RoundedBlue = String(format: "%0.0f",(BlueColor * 255))
+    
+        RedLabel.text = "Red: \(RoundedRed)"
+        GreenLabel.text = "Green: \(RoundedGreen)"
+        BlueLabel.text = "Blue: \(RoundedBlue)"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
